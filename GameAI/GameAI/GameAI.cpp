d164acc9bf714b5b518fe631178292f3c22852df
Sieun <wjstldms0823@naver.com>
2023-10-10 13:03:07 +0900

    [ADD] Project Files BFS, DFS, Dijkstra 실습 안함
    Signed-off-by: Sieun <wjstldms0823@naver.com>

--- GameAI/GameAI/GameAI.cpp
+++ GameAI/GameAI/GameAI.cpp
@@ -1,2 +1,56 @@
 // GameAI.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include "GameAI.h"
+
+
+
+int main() 
+{
+	//templateMethod();
+	//drinkFactory();
+	//trafficLight();
+	westWorld();
+
+	return 0;
+}
+
+void templateMethod() 
+{
+	TemplateMethod* templateMethod = new TemplateMethod();
+	templateMethod->Start();
+}
+
+//void drinkFactory()
+//{
+//	Drinkfactory* drinkfactory = new Drinkfactory();
+//	Drink* coffee = drinkfactory->makeDrink(COFFEE);
+//	cout << coffee->drinkInfo() << endl;
+//}
+
+//void canFactory() 
+//{
+//	CanFactory canfactory;
+//	Can* can = canfactory.createClone(COKE);
+//	can->draw()
+//}
+
+void trafficLight() 
+{
+	TrafficLight* trafficLightFSM = new TrafficLight();
+
+	for (int i = 0; i < 10; i++) 
+	{
+		trafficLightFSM->StartTrafficLight();
+	}
+}
+
+void westWorld() 
+{
+	Miner miner("en_miner_bob");
+
+	for (int i = 0; i < 100; i++) 
+	{
+		miner.Update(); 
+		Sleep(1000); 
+	}
+}
--- GameAI/GameAI/GameAI.h
+++ GameAI/GameAI/GameAI.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <iostream>
+#include "TemplateMethod.h"
+#include "SimpleFactory.h"
+#include "PrototypeDesign.h"
+#include "FSM.h"
+#include "WestWorldProject.h"
+
+using namespace std;
+
+int main();
+void templateMethod();
+//void drinkFactory();
+//void canFactory();
+void trafficLight();
+void westWorld();
